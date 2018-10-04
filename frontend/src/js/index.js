51a4d38fdb00c6ab7642993035fadf45e34b08a8
Pawel Gozdz <pawel.gozdz.web.dev@gmail.com>
2018-10-04 23:44:10 +0200

    All set up. Started with navigation

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,4 +1,7 @@
 import '../scss/main.scss';
+import 'bootstrap';
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown';
 // import '../css/style.css';
 import num from './test';
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // ['babel-polyfill', './src/js/index.js'],
 
@@ -29,11 +29,67 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts/',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        exclude: /fonts/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '/img/[name].[ext]'
+            }
+          }
+        ]
       }
+      // {
+      //   test: /\.(gif|png|jpe?g|svg)$/i,
+      //   exclude: /fonts/,
+      //   use: [
+      //     'file-loader',
+      //     {
+      //       loader: 'image-webpack-loader',
+      //       options: {
+      //         mozjpeg: {
+      //           progressive: true,
+      //           quality: 65
+      //         },
+      //          name: '/img/[name].[ext]'
+      //         // optipng.enabled: false will disable optipng
+      //         optipng: {
+      //           enabled: false,
+      //         },
+      //         pngquant: {
+      //           quality: '65-90',
+      //           speed: 4
+      //         },
+      //         gifsicle: {
+      //           interlaced: false,
+      //         },
+      //         // the webp option will enable WEBP
+      //         webp: {
+      //           quality: 75
+      //         }
+      //       }
+      //     },
+      //   ],
+      // }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin('dist', {}),
+    // new CleanWebpackPlugin('dist', {}),
     // new ExtractTextPlugin(
     //   { filename: 'css/style.[hash].css', disable: false, allChunks: true }
     // ),
